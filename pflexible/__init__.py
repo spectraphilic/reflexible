6f9d6fd8d5f0536b155bb8ed01a8cf404c66715e
Francesc Alted <faltet@gmail.com>
2014-09-26 10:57:01 +0200

    Unique VERSION file added and more files pruned (Cython C output)

--- pflexible/__init__.py
+++ pflexible/__init__.py
@@ -1,1 +1,48 @@
-from pflexible import *
+"""
+SYNOPSIS
+========
+
+    pflexible [-h] [-v,--verbose] [--version]
+
+DESCRIPTION
+===========
+
+    pflexible: A python module for working with FLEXPART Output.
+
+
+EXAMPLES
+========
+
+    #TODO:
+
+    A lot! This is just a starting point. See the doc strings
+    for information about the various functions.
+
+
+AUTHOR
+======
+
+    JFB: John F Burkhart <jfburkhart@gmail.com>
+
+CONTRIBUTORS
+============
+
+    HSO: Harald Sodemann
+    SEC: Sabine Eckhardt
+    AST: Andreas Stohl
+
+    Many functions are adaptations of Fortran / NCARG programs (AST)
+    or Matlab functions (HSO/SEC).
+
+LICENSE
+=======
+
+    This script follows creative commons usage.
+
+"""
+
+from .version import __version__
+
+# Import the public functions here
+from .flexpart_read import read_header
+from .grid_read import read_grid, get_slabs
--- setup.py
+++ setup.py
@@ -7,17 +7,22 @@ from setuptools import Extension
 #from Cython.Distutils import build_ext
 import numpy
 
+# pflexible version
+VERSION = open('VERSION').read().strip()
+# Create the version.py file
+open('pflexible/version.py', 'w').write('__version__ = "%s"\n' % VERSION)
+
 
 setup(
   name = 'pflexible',
-  version = '0.9.0',
+  version = VERSION,
   author = 'John F. Burkhart',
   author_email = 'jfburkhart@gmail.com',
   url = 'http://niflheim.nilu.no/~burkhart/pflexible',
   description = 'A Python interface to FLEXPART data.',
   license = 'Creative Commons',
-  ext_modules = [Extension('pflexcy', ['pflexcy.c'], include_dirs=[numpy.get_include()])],
-  py_modules = ['pflexible','mapping'],
+  ext_modules = [Extension('pflexible.pflexcy', ['pflexible/pflexcy.c'],
+                           include_dirs=[numpy.get_include()])],
+  packages = ['pflexible'],
+  py_modules = ['mapping'],  # XXX that should be converted into a package too?
 )
-
-
