ae305823cdef1aa828857dfc21cc8b41cef465a0
Francesc Alted <faltet at gmail.com>
2014-10-02 13:13:59 +0200

    New uio module to keep all the code specific to UIO.

--- pflexible/data_structures.py
+++ pflexible/data_structures.py
@@ -296,32 +296,6 @@ class Header(Structure):
         """ see :func:`read_trajectories` """
         self.trajectory = pf.read_trajectories(self)
 
-    def add_fires(self, **kwargs):
-        """ uses the :mod:`emissions` module to read the MODIS hotspot data and
-        add it to the header class as a 'fires' attribute.
-
-        **This function is only available within UIO.**
-
-        """
-
-        from jfb.pflexpart import emissions as em
-        self.fires = None
-        for day in self.available_dates_dt:
-            # day = day[:8]
-            firedata = em.MODIS_hotspot(day)
-            daily = firedata.daily
-            # pdb.set_trace()
-            if daily is None:
-                continue
-            else:
-                if self.fires is None:
-                    self.fires = daily
-                else:
-
-                    self.fires = np.hstack(
-                        (self.fires, daily)).view(
-                        np.recarray)
-
     def closest_dates(self, dateval, fmt=None, take_set=False):
         """ given an iterable of datetimes, finds the closest dates.
             if passed a list, assumes it is a list of datetimes
