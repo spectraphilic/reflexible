4a5bc539f6e466bc00b679fe08760b5c4f1cf530
Francesc Alted <faltet@gmail.com>
2014-10-02 16:44:26 +0200

    Moving FortFlex sources into a more significant dirname

--- tiny_test.py
+++ tiny_test.py
@@ -3,5 +3,8 @@ import pflexible as pf
 H = pf.Header('../test_data/')
 print H.keys()
 
-print "Calling fill_backward():"
-H.fill_backward()
+#print "Calling fill_backward():"
+#H.fill_backward()
+
+print "Calling read_grid():"
+FD = pf.read_grid(H, time_ret=0,nspec_ret=0)
