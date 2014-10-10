5ac16416ec6fe40bb4695b910bca65c7570f62c5
Francesc Alted <faltet@gmail.com>
2014-10-10 12:37:21 +0200

    Simplified the all.py test runner

--- pflexible/tests/all.py
+++ pflexible/tests/all.py
@@ -2,7 +2,6 @@
 Run all test cases.
 """
 
-import sys
 import os
 import unittest
 
@@ -15,24 +14,15 @@ def suite():
         start_dir=this_dir, pattern="test_*.py")
 
 
-def test(only_versions=False):
+def test():
     """
     test()
 
     Run all the tests in the test suite.
     """
     pf.print_versions()
-    if not only_versions:
-        return unittest.TextTestRunner().run(suite())
+    return unittest.TextTestRunner().run(suite())
 
 
 if __name__ == '__main__':
-
-    # Handle some global flags (i.e. only useful for test_all.py)
-    only_versions = False
-    args = sys.argv[:]
-    for arg in args:
-        if arg in ['-print-versions']:
-            only_versions = True
-            sys.argv.remove(arg)
-    test(only_versions)
+    test()
