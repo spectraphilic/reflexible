8a0b1fae9403551fbb7faf65fbca6824f4bf8b9c
John F. Burkhart <jfburkhart@gmail.com>
2014-11-14 11:11:19 +0100

    added a backward run test case

--- pflexible/tests/test_api.py
+++ pflexible/tests/test_api.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import pflexible as pf
 
 
-class Test_API(TestCase):
+class Test_API_Fwd(TestCase):
 
     H = pf.Header(pf.Fwd1_data)
     fdkeys = [
@@ -37,3 +37,31 @@ class Test_API(TestCase):
         assert tkeys == [
             'RELEASE_TEST1', 'Trajectories', 'date', 'info', 'labels',
             'version']
+
+class Test_API_Bwd(TestCase):
+
+    H = pf.Header(pf.Bwd1_data)
+    fdkeys = [
+            'dry', 'grid', 'gridfile', 'itime', 'max', 'min', 'rel_i',
+            'shape', 'slabs', 'spec_i', 'species', 'timestamp', 'wet']
+
+    def test_fill_backward(self):
+        self.H.fill_backward(nspec=(0,))
+        ckeys = self.H.C[(0,0)].keys()
+        assert sorted(ckeys) == self.fdkeys
+        fdkeys = self.H.FD[(0, self.H.available_dates[0])].keys()
+        assert sorted(fdkeys) == self.fdkeys
+
+    def test_read_grid(self):
+        FD = pf.read_grid(self.H, time_ret=0, nspec_ret=0)
+        fdkeys = sorted(FD.keys())
+        assert fdkeys == ['grid_dates', 'options', (0, self.H.available_dates[0])]
+        fdkeys_ = sorted(FD[(0, self.H.available_dates[0])].keys())
+        assert fdkeys_ == self.fdkeys
+
+    def test_H_read_grid(self):
+        self.H.read_grid(time_ret=0, nspec_ret=0)
+        fdkeys = sorted(self.H.FD.keys())
+        assert fdkeys == ['grid_dates', 'options', (0, self.H.available_dates[0])]
+        fdkeys_ = sorted(self.H.FD[(0, self.H.available_dates[0])].keys())
+        assert fdkeys_ == self.fdkeys
--- tiny_test.py
+++ tiny_test.py
@@ -20,3 +20,23 @@ FD = pf.read_grid(H, time_ret=0,nspec_ret=0)
 T = pf.read_trajectories(H)
 print "T.keys():"
 print T.keys()
+
+# Backward Runs
+#
+H = pf.Header(pf.Bwd1_data)
+print H.keys()
+
+print "Calling fill_backward():"
+H.fill_backward(nspec=(0,))
+
+# H.C should be available by now
+print "H.C.keys():"
+print H.C[(0,0)].keys()
+
+# H.FD should be available by now
+print "H.FD.keys():"
+print H.FD[(0, H.available_dates[0])].keys()
+
+print "Calling read_grid():"
+FD = pf.read_grid(H, time_ret=0,nspec_ret=0)
+
