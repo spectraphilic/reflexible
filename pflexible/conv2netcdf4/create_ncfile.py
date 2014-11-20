10753b0f390959d658ae855e6d7f0935ce65728c
Francesc Alted <faltet@gmail.com>
2014-11-20 17:33:12 +0100

    Added a more complete set of attributes.  Still many that are not in header.

--- pflexible/conv2netcdf4/create_ncfile.py
+++ pflexible/conv2netcdf4/create_ncfile.py
@@ -23,9 +23,46 @@ def write_metadata(H, ncid):
     ncid.Conventions = 'CF-1.6'
     ncid.title = 'FLEXPART model output'
     # ncid.institution = institution.strip()
-    # ncid.source = trim(flexversion) + ' model output'
+    ncid.source = H.version + ' model output'
     # ncid.history = date[:4] + '-' + date[4:6] + '-' + date[6:8] + ' ' + time[0:2] + ':' + time(3:4) + ' ' + zone + '  created by '+ trim(login_name) + ' on ' + trim(host_name)
     ncid.references = 'Stohl et al., Atmos. Chem. Phys., 2005, doi:10.5194/acp-5-2461-200'
+
+    # attributes describing model run
+    ncid.outlon0 = H.outlon0
+    ncid.outlat0 = H.outlat0
+    ncid.dxout = H.dxout
+    ncid.dyout = H. dyout
+
+    # COMMAND file settings
+    ncid.ibdate = H.ibdate
+    ncid.ibtime = H.ibtime
+    # ncid.iedate = H.iedate    # XXX not present in H header
+    # ncid.ietime = H.ietime    # XXX not present in H header
+    ncid.loutstep = H.loutstep
+    ncid.loutaver = H.loutaver
+    ncid.loutsample = H.loutsample
+    ncid.lsubgrid = H.lsubgrid
+    ncid.lconvection = H.lconvection
+    ncid.ind_source = H.ind_source
+    ncid.ind_receptor = H.ind_receptor
+
+    # From here on, the COMMAND settings are not present in H header
+    # ncid.ldirect = H.ldirect
+    # ncid.itsplit = H.itsplit
+    # ncid.linit_cond = H.linit_cond
+    # ncid.lsynctime = H.lsynctime
+    # ncid.ctl = H.ctl
+    # ncid.ifine = H.ifine
+    # ncid.iout = H.iout
+    # ncid.ipout = H.ipout
+    # ncid.lagespectra = H.lagespectra
+    # ncid.ipin = H.ipin
+    # ncid.ioutputforeachrelease = H.ioutputforeachrelease
+    # ncid.iflux = H.iflux
+    # ncid.mdomainfill = H.mdomainfill
+    # ncid.mquasilag = H.mquasilag
+    # ncid.nested_output = H.nested_output
+    # ncid.surf_only = H.surf_only
 
 
 def create_ncfile(H, ncfile):
